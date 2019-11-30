4ae66899394c4b73135d61c86791a74c51c3fc7f
gonzoblasco <54Gb2406>
2019-11-30 20:41:31 -0300

    Header Text Update

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,9 @@ import React from "react";
 const Header = () => (
   <header className="uk-margin" uk-margin="true">
     <h1 className="uk-text-center">Eventos en React con EventBrite API</h1>
+    <p className="uk-text-center">
+      La API que se utiliza no funciona bien, muestra resultados una de cada ocho búsquedas o menos...
+    </p>
   </header>
 );
 
